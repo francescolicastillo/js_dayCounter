ed91669b7a6143a7fbc5de279a808a875e2c59ae
fcastillo <frank_hdz16@hotmail.com>
2023-10-14 23:30:22 +0200

    JS: intervat function implemented

--- main.js
+++ main.js
@@ -17,11 +17,6 @@ const handlerInputSpace = (e) => {
 
 function addEvent() {
     if(isValid()){
-        const temo = new Date(eventDate.value);
-        const tt = new Date(Date.now());
-        console.log(temo);
-        console.log(tt);
-        console.log(temo - tt);
         const event = {
             titleEvent: eventInput.value,
             dateEvent: eventDate.value,
@@ -44,19 +39,50 @@ const cleanState = () => {
 
 const setEventHTML = (event) => {
     const divContainer = document.createElement("div");
+    divContainer.setAttribute("id", event.idEvent);
+
     const divTimer = document.createElement("div");
+    divTimer.setAttribute("class", "restTime");
+    const nodeTimer = document.createTextNode(new Date(event.dateEvent) - new Date(Date.now()));
+    divTimer.appendChild(nodeTimer);
     
     const divDate = document.createElement("div");
+    divDate.setAttribute("class", "dateEvent");
     const nodeTime = document.createTextNode(event.dateEvent);
-    divTimer.appendChild(nodeTime);
+    divDate.appendChild(nodeTime);
 
     const para = document.createElement("p");
+    para.setAttribute("class", "titleEvent");
     const node = document.createTextNode(event.titleEvent);
     para.appendChild(node);
+
+    divContainer.appendChild(divTimer);
     divContainer.appendChild(divDate);
     divContainer.appendChild(para);
 
     eventsList.appendChild(divContainer);
+    startInterval(event.idEvent);
+}
+
+const startInterval = (id) => {
+    const htmlElement = document.getElementById(id);
+    let restTime = htmlElement.querySelector(".restTime");
+    console.log(restTime);
+    const intervalId = setInterval(() => {
+        restTime.innerHTML -= 1;
+        console.log(restTime);
+
+        // timer.innerHTML = (
+        //     (Math.floor(counter/60) < 10 ? ("0" + Math.floor(counter/60)) : Math.floor(counter/60))
+        //     + ":" 
+        //     + ((counter%60) < 10 ? ("0"+(counter%60)) : (counter%60))
+        // );
+        if(restTime === 0) {
+            // clearInterval(intervalId);
+            // setFinishedTasks();
+            console.log("finished");
+        }
+    }, 1000);
 }
 
 
